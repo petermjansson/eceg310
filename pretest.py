160db045f4887953cfac347953179a2a4c972b41
petermjansson <52827420+petermjansson@users.noreply.github.com>
2020-11-15 21:42:13 -0500

    Update pretest.py

--- pretest.py
+++ pretest.py
@@ -8,13 +8,14 @@
 import os
 import sys
 import numpy as np
+# import matplotlib.pyplot as plt
 from time import gmtime, sleep, strftime, time
 
 # pull in the daqhat library
 from daqhats import hat_list, HatIDs, mcc118
 
 
-class PretestDataLogger:
+class ShortSampleDataLogger:
     '''
     Scan the inputs on one or more MCC 118 boards and log data to a file
     in a loop (unitl interrupted) using the log method.
@@ -43,30 +44,36 @@ class PretestDataLogger:
         self.running = False
 
 
-    def log(self, testVal):
+    def log(self):
         '''
         In a loop (until interrupted) scan the inputs of all detected boards
         and output data to a tab delimited text file with a single header row
         '''
 
-        # get one line of voltage data
-        testVal = np.zeros((1, 64))
+        # get desired number of lines of voltage data
+        num_samples = 40
+        testVal = np.zeros((num_samples, 64))
+        # intialize counter, index, and running boolean
         idx = 0
         tmpCounter = 0
         self.running = True
         while self.running:
             # Read and display every channel
-            #for entry in self.boards:
+            # for entry in self.boards:
             for board in self.boardsEntry:
                 for channel in range(board.info().NUM_AI_CHANNELS):
-                    testVal[0,idx] = "%.4f" % board.a_in_read(channel)
+                    testVal[tmpCounter,idx] = "%.4f" % board.a_in_read(channel)
                     idx += 1
         
             sleep(0.5)
+            # iterate counter
             tmpCounter += 1
-            # break condition
-            if tmpCounter == 1:
+            # reset index
+            idx = 0
+            # break condition based on number of lines
+            if tmpCounter == num_samples:
                 self.running = False
+        # print(testVal)
         return testVal
 
 # Main program starts here. Use `python3 log2.py` to run
@@ -75,11 +82,8 @@ if __name__ == "__main__":
     # Pull in python standard libraries only used when running as program
     import signal
 
-   # Create Data Logger
-    logger = PretestDataLogger()
-
-    # empty input matrix
-    testVal = []
+    # Create Data Logger
+    logger = ShortSampleDataLogger()
 
     # Add signal handler so systemd can shutdown service
     signal.signal(signal.SIGINT, logger.handle_interupt)
@@ -90,7 +94,6 @@ if __name__ == "__main__":
     # get shape of voltages
     m = val.shape[0]
 
-
     # Impulse response of the filter
     IR = np.ones((5,)) / 5
 
@@ -99,19 +102,19 @@ if __name__ == "__main__":
 
     # Voltages for 1st arm read (1st arm being Alpha) in matrix form
     # initialize matricies for each arm
-    alpha = np.zeros((7,m))
-    beta = np.zeros((7,m))
-    charlie = np.zeros((7,m))
-    delta = np.zeros((7,m))
-    echo = np.zeros((7,m))
-    foxtrot = np.zeros((7,m))
-    golf = np.zeros((7,m))
-    hotel = np.zeros((7,m))
+    alpha = np.zeros((6,m))
+    beta = np.zeros((6,m))
+    charlie = np.zeros((6,m))
+    delta = np.zeros((6,m))
+    echo = np.zeros((6,m))
+    foxtrot = np.zeros((6,m))
+    golf = np.zeros((6,m))
+    hotel = np.zeros((6,m))
 
     # add voltages to each row
     for i in range(alpha.shape[0]):
         alpha[i] = val[:,volt_ord[7-i]]-val[:,volt_ord[6-i]]
-        beta[i] = val[:,volt_ord[7-i]+8]-val[:,volt_ord[6-i]+8]
+        bravo[i] = val[:,volt_ord[7-i]+8]-val[:,volt_ord[6-i]+8]
         charlie[i] = val[:,volt_ord[7-i]+16]-val[:,volt_ord[6-i]+16]
         delta[i] = val[:,volt_ord[7-i]+24]-val[:,volt_ord[6-i]+24]
         echo[i] = val[:,volt_ord[7-i]+32]-val[:,volt_ord[6-i]+32]
@@ -119,23 +122,11 @@ if __name__ == "__main__":
         golf[i] = val[:,volt_ord[7-i]+48]-val[:,volt_ord[6-i]+48]
         hotel[i] = val[:,volt_ord[7-i]+56]-val[:,volt_ord[6-i]+56]
 
-        # make the current (I) row
-        if i == 6:
-            alpha[i] = (val[:,volt_ord[7-i]]-val[:,volt_ord[6-i]]) / 0.1
-            beta[i] = (val[:,volt_ord[7-i]+8]-val[:,volt_ord[6-i]+8]) / 0.1
-            charlie[i] = (val[:,volt_ord[7-i]+16]-val[:,volt_ord[6-i]+16]) / 0.1
-            delta[i] = (val[:,volt_ord[7-i]+24]-val[:,volt_ord[6-i]+24]) / 0.1
-            echo[i] = (val[:,volt_ord[7-i]+32]-val[:,volt_ord[6-i]+32]) / 0.1
-            foxtrot[i] = (val[:,volt_ord[7-i]+40]-val[:,volt_ord[6-i]+40]) / 0.1
-            golf[i] = (val[:,volt_ord[7-i]+48]-val[:,volt_ord[6-i]+48]) / 0.1
-            hotel[i] = (val[:,volt_ord[7-i]+56]-val[:,volt_ord[6-i]+56]) / 0.1
-
-
     # Convolving the sample for every battery to apply the average running filter
     # Also truncating the convolved series to match the battery series
     for i in range(alpha.shape[0]):
         alpha[i] = np.convolve(alpha[i],IR)[:m]
-        beta[i] = np.convolve(beta[i],IR)[:m]
+        bravo[i] = np.convolve(bravo[i],IR)[:m]
         charlie[i] = np.convolve(charlie[i],IR)[:m]
         delta[i] = np.convolve(delta[i],IR)[:m]
         echo[i] = np.convolve(echo[i],IR)[:m]
@@ -144,57 +135,53 @@ if __name__ == "__main__":
         hotel[i] = np.convolve(hotel[i],IR)[:m]
 
 
-    # sample for 20 - 30 seconds
-    # then take the average of those samples for each battery
+    # Get average voltages for each battery
+    # intialize empty average voltage matrix
+    avg_volts = np.zeros((8,6))
+    
+    # make a voltage sample list
+    volt_smpls = [alpha, bravo, charlie, delta, echo, foxtrot, golf, hotel]
+    
+    # make an index for arm names
+    arm_names = ['Alpha', 'Bravo', 'Charlie', 'Delta', 'Echo', 'Foxtrot', 'Golf', 'Hotel']
+    
+    # take the mean of the voltage for each battery
+    for i in range(8):
+        avg_volts[i] = np.mean(volt_smpls[i], axis = 1)
+        
+    # find the mean of the battery voltages
+    mean = np.mean(avg_volts)
 
-    # make one array for all average voltages
-    battery_volt = np.concatenate((alpha[:,0], beta[:,0], charlie[:,0], delta[:,0], \
-        echo[:,0], foxtrot[:,0], golf[:,0], hotel[:,0]), axis=None)
+    # find the standard deviation of the battery voltages
+    std = np.std(avg_volts,dtype=np.float64)
+
 
     # ================= #
-    # DEAD BATTERY TEST #
+    # LOW BATTERY TEST #
     # ================= #
+
+    for i in range(avg_volts.shape[0]):
+        for j in range(avg_volts.shape[1]):
+            if avg_volts[i,j] <= 1.3:
+                print(arm_names[i], j + 1, "needs to be charged")
+
 
     # ===================== #
     # Reversed Battery Test #
     # ===================== #
 
-    # find the mean of the battery voltages
-    mean = np.mean(battery_volt)
+    for i in range(avg_volts.shape[0]):
+        for j in range(avg_volts.shape[1]):
+            if avg_volts[i,j] < 0:
+                print(arm_names[i], j + 1, "is reversed")
 
-    # find the standard deviation of the battery voltages
-    std = np.std(battery_volt,dtype=np.float64)
 
     # ========================= #
     # Battery Out of Range Test #
     # ========================= #
 
-
-    # Battery out of range
-    for i in range(7):
-        # redefine based on average values
-        for j in range(1):
-            if alpha[i,j] not in range(mean - 3 * std, mean + 3 * std):
-                print("alpha", i + 1, "out of range")
-                alpha_clear = False
-            if beta[i,j] <= 0.1:
-                print("beta", i + 1, "battery low")
-                beta_clear = False
-            if charlie[i,j] <= 0.1:
-                print("charlie", i + 1, "battery low")
-                charlie_clear = False
-            if delta[i,j] <= 0.1:
-                print("delta", i + 1, "battery low")
-                delta_clear = False
-            if echo[i,j] <= 0.1:
-                print("echo", i + 1, "battery low")
-                echo_clear = False
-            if foxtrot[i,j] <= 0.1:
-                print("foxtrot", i + 1, "battery low")
-                foxtrot_clear = False
-            if golf[i,j] <= 0.1:
-                print("golf", i + 1, "battery low")
-                golf_clear = False
-            if hotel[i,j] <= 0.1:
-                print("hotel", i + 1, "battery low")
-                hotel_clear = False
+    for i in range(avg_volts.shape[0]):
+        for j in range(avg_volts.shape[1]):
+            if mean - 3 * std >= avg_volts[i,j] >= mean + 3 * std:
+                print(arm_names[i], j + 1, "out of range")
+                    
